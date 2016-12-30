ffd47488b66bf3f735e610f96fab3d5423433648
oskimura <oskimura@gmail.com>
2016-12-30 20:14:20 +0900

    bug fix counter

--- src/counter.v
+++ src/counter.v
@@ -1,11 +1,17 @@
-module counter(clk,out);
+module counter(in,ld,clk,out);
+    input [3:0] in;
+    input ld;
     input clk;
     output [3:0] out;
 
     reg [3:0] cnt;
     always @(posedge clk) begin
-        cnt <= cnt+1;  
+        if (ld==0) begin
+            cnt <= in;
+        end else begin
+            cnt <= cnt+1; 
+        end 
     end
     assign out = cnt;
-    
+
 endmodule
