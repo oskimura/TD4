f4add0c6a1cde855200d82e289761b5cf1bc74d2
oskimura <oskimura@gmail.com>
2016-12-30 20:13:29 +0900

    add carry flag alu

--- src/alu.v
+++ src/alu.v
@@ -1,6 +1,7 @@
-module alu(ain, bin, out);
+module alu(ain, bin, c, out);
     input wire [3:0] ain,bin;
+    output c;
     output wire [3:0] out;
 
-    assign out = ain + bin;
+    assign {c,out} = ain + bin;
 endmodule
